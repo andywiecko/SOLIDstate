b1be8bbbefff650ef960cb01776ba02866599e97
andywiecko <awiecko@us.edu.pl>
2019-10-08 18:36:49 +0200

    one/two body int. moved imp. from .hpp to .cpp

--- Modules/Terms/OneBodyInteraction.cpp
+++ Modules/Terms/OneBodyInteraction.cpp
@@ -1,1 +1,35 @@
 #include "OneBodyInteraction.hpp"
+
+template <template<typename> class T1, typename T2>
+void OneBodyInteractionTermFermions::FillElements(QuantumSystem<T1, T2> &qSystem)
+{
+	int k = qSystem.hilbertSpace.stateTotalIndex;
+	bool N05option = qSystem.hamiltonian.termsEnabled.N05option;
+	BaseState baseState = qSystem.hilbertSpace.baseState;
+    auto M = qSystem.parameters['M'];
+	double Hij = 0;
+	for (int i = 0; i < baseState.size(); i++)
+	{
+		if (baseState.OneBodyInteraction(i))
+			Hij += M(i);
+		if (N05option)
+			Hij -= 0.5 * M(i);
+	}
+	qSystem.hamiltonian.matrixElements(k, k) += Hij;
+}
+
+template <template<typename> class T1, typename T2>
+void OneBodyInteractionTermSpins::FillElements(QuantumSystem<T1, T2> &qSystem)
+{
+	int k = qSystem.hilbertSpace.stateTotalIndex;
+	for (int i = 0; i < qSystem.hilbertSpace.baseState.size(); i++)
+	{
+		if (qSystem.hilbertSpace.baseState.OneBodyInteraction(i))
+			qSystem.hamiltonian.matrixElements(k, k) += qSystem.parameters['M'](i);
+		else
+			qSystem.hamiltonian.matrixElements(k, k) -= qSystem.parameters['M'](i);
+	}
+}
+
+template void OneBodyInteractionTermFermions::FillElements<Mat,double>(QuantumSystem<Mat,double> &qSystem);
+template void OneBodyInteractionTermSpins::FillElements<Mat,double>(QuantumSystem<Mat,double> &qSystem);
--- Modules/Terms/TwoBodyInteraction.hpp
+++ Modules/Terms/TwoBodyInteraction.hpp
@@ -20,37 +20,4 @@ public:
 	static void FillElements(QuantumSystem<T1, T2> &system);
 };
 
-template <template<typename> class T1, typename T2>
-void TwoBodyInteractionTermFermions::FillElements(QuantumSystem<T1, T2> &qSystem)
-{
-    
-	int k = qSystem.hilbertSpace.stateTotalIndex;
-	bool N05option = qSystem.hamiltonian.termsEnabled.N05option;
-	BaseState baseState = qSystem.hilbertSpace.baseState;
-    arma::SpMat<T2> V = qSystem.parameters['V'];
-    auto start = V.begin();
-    auto end = V.end();
-    T2 Hij = 0;
-	for (auto it=start;it!=end;++it ) 
-	{
-        int i = it.row();
-        int j = it.col();
-		if (baseState.TwoBodyInteraction(i,j))
-            Hij += V(i,j);
-		if (N05option)
-		{
-			if (baseState.OneBodyInteraction(i)) Hij -= 0.5*V(i,j);
-			if (baseState.OneBodyInteraction(j)) Hij -= 0.5*V(i,j);
-			Hij += 0.25 * V(i,j);
-		}
-	}
-    qSystem.hamiltonian.matrixElements(k, k) += Hij;
-}
-
-template <template<typename> class T1, typename T2>
-void TwoBodyInteractionTermSpins::FillElements(QuantumSystem<T1, T2> &qSystem)
-{
-	//TODO
-}
-
 #endif
