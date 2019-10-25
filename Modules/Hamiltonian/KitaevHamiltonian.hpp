d7210ec6ad1c4c8bcee451cb5ab932992015a121
andywiecko <awiecko@us.edu.pl>
2019-10-25 22:46:07 +0200

    Kitaev hamiltonian description

--- Modules/Hamiltonian/KitaevHamiltonian.hpp
+++ Modules/Hamiltonian/KitaevHamiltonian.hpp
@@ -16,6 +16,20 @@
 namespace solid
 {
 
+/**
+ * @brief [Kitaev Hamiltonian](https://arxiv.org/pdf/cond-mat/0010440.pdf) implementation
+ * 
+ * @tparam T1 matrix type: arma::Mat and arma:SpMat are supported
+ * @tparam T2 data type: double, std::complex<double> are supported
+ * 
+ * Kitaev Hamiltonian extended by the many-body interaction.
+ * Model can be described by the following Hamiltonian:
+ * \f[ \hat H = \sum_{ij}\left(-t_{ij}a_i^\dagger a_j +\text{H.c.}\right)
+ *              +\sum_{ij}\left(\Delta_{ij}a_i^\dagger a_j^\dagger +\text{H.c.}\right)
+ *              +\sum_{ij}V_{ij}\hat n_i \hat n_j
+ * 				+\sum_i \mu_i \hat n_i
+ *   \f]
+ */
 template <template <typename> class T1, typename T2>
 class KitaevHamiltonian : public Operator<T1, T2>, public IHamiltonian
 {
