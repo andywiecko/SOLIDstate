f6aefa25b48748ba70caf69bf5b2db1c8ee64d2d
andywiecko <andrzej_wieckowski@o2.pl>
2019-10-29 12:45:20 +0100

    lab description added

--- Modules/Laboratory/Laboratory.hpp
+++ Modules/Laboratory/Laboratory.hpp
@@ -8,8 +8,23 @@
 
 namespace solid
 {
+    /**
+     * @brief contains static methods for measurement
+     * 
+     */
     class Laboratory
     {
+        /**
+         * @brief measures dot product between QuantumState and QuantumSystem
+         * 
+         * @tparam T1 matrix type: arma::Mat and arma:SpMat are supported
+         * @tparam T2 data type: double, std::complex<double> are supported
+         * @return T2 returns scalar product < QuamtumState | QuantumSystem | QuantumState >
+         * 
+         * Returns \f$\langle \psi | \hat {\mathcal O} | \psi \rangle \f$, 
+         * where \f$|\psi\rangle\f$ is QuantumState 
+         * and \f$\hat {\mathcal O}\f$ are current matrixElements in QuantumSystem
+         */
         template <template <typename> class T1, typename T2>
         static T2 Measure(QuantumSystem<T1,T2> &, QuantumState<T2> &);
 
