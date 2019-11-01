addee77ebcca443f883567d4e8e1ea95303c6f97
andywiecko <awiecko@us.edu.pl>
2019-11-01 18:05:53 +0100

    an example with doxygen @pages

--- Modules/SOLIDstate.hpp
+++ Modules/SOLIDstate.hpp
@@ -53,6 +53,38 @@
 #include "Parameters/Parameters.hpp"
 
 /**
+ * @mainpage SOLIDstate: C++ library
+ * 
+ * This project helps user to draw shapes.
+ * Currently two types of shapes can be drawn:
+ * - @subpage drawingRectanglePage "How to draw rectangle?"
+ *
+ * - @subpage drawingCirclePage "How to draw circle?"
+ *
+ */ 
+
+/** @page drawingRectanglePage How to draw rectangle?
+ *
+ * Lorem ipsum dolor sit amet
+ *
+ */
+
+/** @page drawingCirclePage How to draw circle?
+ *
+ * This page is about how to draw a circle.
+ * Following sections describe circle:
+ * - @ref groupCircleDefinition "Definition of Circle"
+ * - @ref groupCircleClass "Circle Class"
+ */
+
+/** @defgroup groupCircleDefinition Circle Definition
+ * A circle is a simple shape in Euclidean geometry.
+ * It is the set of all points in a plane that are at a given distance from a given point, the centre;
+ * equivalently it is the curve traced out by a point that moves so that its distance from a given point is constant.
+ * The distance between any of the points and the centre is called the radius.
+ */
+
+/**
  * @brief SOLIDstate namespace
  */
 namespace solid{}
