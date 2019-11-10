d12306b431d150606717b8c90c07ef257c44c3f1
andywiecko <awiecko@us.edu.pl>
2019-11-10 13:30:56 +0100

    some clean up pt 3

--- Modules/MatrixElementFiller/TermsEnumConverter-imp.cpp
+++ Modules/MatrixElementFiller/TermsEnumConverter-imp.cpp
@@ -9,13 +9,13 @@
  * 
  */
 #include "TermsEnumConverter.cpp"
-#include<armadillo>
+
 namespace solid
 {
 
-template mapTermsFunction<arma::Mat, double> TermsEnumConverter<arma::Mat,double>::dict;
-template mapTermsFunction<arma::Mat, arma::cx_double> TermsEnumConverter<arma::Mat,arma::cx_double>::dict;
-template mapTermsFunction<arma::SpMat, double> TermsEnumConverter<arma::SpMat,double>::dict;
-template mapTermsFunction<arma::SpMat, arma::cx_double> TermsEnumConverter<arma::SpMat,arma::cx_double>::dict;
+template mapTermsFunction<arma::Mat, double> TermsEnumConverter<arma::Mat, double>::dict;
+template mapTermsFunction<arma::Mat, arma::cx_double> TermsEnumConverter<arma::Mat, arma::cx_double>::dict;
+template mapTermsFunction<arma::SpMat, double> TermsEnumConverter<arma::SpMat, double>::dict;
+template mapTermsFunction<arma::SpMat, arma::cx_double> TermsEnumConverter<arma::SpMat, arma::cx_double>::dict;
 
-}
+} // namespace solid
