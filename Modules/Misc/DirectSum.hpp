1172e8919d4ece6801541d564e00139e34e06645
andywiecko <awiecko@us.edu.pl>
2019-11-29 14:56:46 +0100

    doxy + directsum updated

--- Modules/Misc/DirectSum.hpp
+++ Modules/Misc/DirectSum.hpp
@@ -7,6 +7,24 @@
 namespace solid
 {
 
+/**
+ * @brief Direct sum of two matrices
+ * 
+ * It returns the following matrix product:
+ * \f[
+ * A\oplus B = 
+ * \left[
+ * \begin{array}{cc} 
+ *  A & 0 \\ 0 & B
+ * \end{array}\right]
+ * \f]
+ * 
+ * @tparam T1 matrix type: arma::Mat and arma:SpMat are supported
+ * @tparam T2 data type: double, std::complex<double> are supported
+ * @param A upper matrix
+ * @param B  lower matrix
+ * @return T1<T2> direct sum \f$A\oplus B\f$
+ */
 template <template <typename> class T1, typename T2>
 T1<T2> DirectSum(T1<T2> A, T1<T2> B)
 {
